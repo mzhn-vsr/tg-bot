e913ecdea7095b08243e4e9d1fc8c586fd43a4cb
StarPandaBeg <kshmurakov@mail.ru>
2024-09-28 23:18:22 +0300

    feat: add stats

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -1,15 +1,45 @@
 import client from "../util/client";
 
+export interface ResponseMessage {
+  feedbackId: string;
+  answer: string;
+}
+
+export interface ResponseStats {
+  positive: number;
+  negative: number;
+  total: number;
+}
+
 /** Send message to external AI api
  *
  * @throws `Error` when request failed on any reason
  */
 export async function sendMessage(message: string) {
-  const response = await client.post("/predict", {
+  const response = await client.post("/invoke", {
     question: message,
   });
 
   if (response.status != 200)
     throw new Error("Unable to make request", response.data);
-  return response.data.answer;
+
+  return response.data as ResponseMessage;
+}
+
+export async function sendStatus(messageId: string, status: boolean) {
+  const response = await client.put("/feedback", {
+    id: messageId,
+    isUseful: status,
+  });
+
+  if (response.status != 200)
+    throw new Error("Unable to make request", response.data);
+}
+
+export async function getStats() {
+  const response = await client.get("/feedback");
+
+  if (response.status != 200)
+    throw new Error("Unable to make request", response.data);
+  return response.data as ResponseStats;
 }
--- src/bot/actions/chat.ts
+++ src/bot/actions/chat.ts
@@ -1,23 +1,40 @@
 import { Context } from "telegraf";
 import { button, inlineKeyboard } from "telegraf/markup";
 import { Action } from ".";
-import { sendMessage } from "../../api/chat";
+import { getStats, sendMessage, sendStatus } from "../../api/chat";
 import { _ } from "../../locale";
 import logger from "../../util/log";
-
-const voteKeyboard = inlineKeyboard([
-  button.callback("👍", "good"),
-  button.callback("👎", "bad"),
-]);
 
 const trySendMessage = async (text: string, ctx: Context) => {
   await ctx.sendChatAction("typing");
   const response = await sendMessage(text);
 
-  ctx.reply(response, { reply_markup: voteKeyboard.reply_markup });
+  const voteKeyboard = inlineKeyboard([
+    button.callback("👍", `good_${response.feedbackId}`),
+    button.callback("👎", `bad_${response.feedbackId}`),
+  ]);
+
+  ctx.reply(response.answer, { reply_markup: voteKeyboard.reply_markup });
 };
 
 export const chatAction: Action = async (bot) => {
+  bot.command("stats", async (ctx) => {
+    try {
+      const response = await getStats();
+      ctx.reply(
+        _("message_stats", {
+          total: response.total,
+          good: response.positive,
+          bad: response.negative,
+        }),
+        { parse_mode: "HTML" }
+      );
+    } catch (e) {
+      logger.error(e);
+      ctx.reply(_("message_server_error"));
+    }
+  });
+
   bot.on("message", async (ctx) => {
     if (ctx.text == undefined || ctx.text == "") {
       ctx.replyWithMarkdownV2(_("message_unknown_payload"));
@@ -30,5 +47,24 @@ export const chatAction: Action = async (bot) => {
       ctx.reply(_("message_server_error"));
       logger.error(e);
     }
+  });
+
+  bot.on("callback_query", async (ctx) => {
+    // @ts-ignore
+    const [status, id] = ctx.callbackQuery.data.split("_");
+    const isGood = status === "good";
+
+    const statusString = isGood ? "хорошую" : "плохую";
+
+    try {
+      await sendStatus(id, isGood);
+    } catch (e) {
+      logger.error(e);
+    }
+
+    ctx.editMessageText(
+      ctx.text + `\n\n` + _("message_vote", { vote: statusString }),
+      { parse_mode: "HTML", reply_markup: inlineKeyboard([]).reply_markup }
+    );
   });
 };
