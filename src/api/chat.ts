e1d19e49fc885b2e9cbd675e06f05cc20f763965
StarPandaBeg <kshmurakov@mail.ru>
2024-09-28 15:49:32 +0300

    feat: update api

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -1,17 +1,15 @@
 import client from "../util/client";
 
 /** Send message to external AI api
- * 
+ *
  * @throws `Error` when request failed on any reason
  */
 export async function sendMessage(message: string) {
-  const response = await client.post("/chat/invoke", {
+  const response = await client.post("/predict", {
     input: message,
-    config: {},
-    kwargs: {},
   });
 
   if (response.status != 200)
     throw new Error("Unable to make request", response.data);
-  return response.data.output.content;
+  return response.data.answer;
 }
